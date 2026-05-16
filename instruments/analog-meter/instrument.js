1596b21dea35976a3a34ec13167ada06749d8e3e
Marek Sterzik <marek.sterzik@gmail.com>
2026-05-16 13:53:32 +0200

    css supports asset links

---
+++ instruments/analog-meter/instrument.js
@@ -0,0 +1,17 @@
+function initialize(instrument)
+{
+    console.log("initialize")
+}
+
+function setValue(instrument, value)
+{
+    console.log("setValue", value)
+}
+
+function destroy(instrument)
+{
+    console.log("setValue")
+
+}
+
+export {initialize, setValue, destroy}
